edb9f34e366ea6ad51f257458ee9d7c2235740d5
Luke Vivier <luke@vivier.ca>
2015-06-25 23:31:39 -0700

    docblock

--- index.js
+++ index.js
@@ -5,28 +5,37 @@
 
 var jsonp = require('jsonp')
 
+/**
+ * Find a GitHub user by email or username
+ *
+ * @param {string} str
+ * @param {function} cb
+ */
+
 module.exports = function (str, cb) {
-  (str.match(/[^@]+@[\w.-]+/)? email : profile)(str, cb)
+  (str.match(/[^@]+@[\w.-]+/)? getByEmail : getByUser)(str, cb)
 }
 
 /**
  * GitHub user by email
+ * @api private
  */
 
-function email (email, cb) {
+function getByEmail (email, cb) {
   var url = 'https://api.github.com/search/users?q='+email+' in:email'
   jsonp(url, function (err, res) {
     if (err) return cb(err)
-    if (res && res.data && res.data.total_count) return profile(res.data.items[0].login, cb)
+    if (res && res.data && res.data.total_count) return getByUser(res.data.items[0].login, cb)
     cb(new Error('no user with email '+email))
   })
 }
 
 /**
  * GitHub user profile
+ * @api private
  */
 
-function profile (name, cb) {
+function getByUser (name, cb) {
   var url = 'https://api.github.com/users/'+name
   jsonp(url, function (err, res) {
     if (err) return cb(err)
